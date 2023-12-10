d37ff6ae5d5f4a020a69411632cefafe0f462251
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-12-10 14:43:27 +0900

    feat(RootLayout) : tanstackquery/chakra

---
+++ src/app/ChakraLayout.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ColorModeScript, ThemeConfig, extendTheme } from "@chakra-ui/react";
+
+import dynamic from "next/dynamic";
+const ChakraProvider = dynamic(() =>
+  import("@chakra-ui/provider").then((mod) => mod.ChakraProvider)
+);
+import localFont from "next/font/local";
+
+// const BMJUA = localFont({
+//   src: [
+//     {
+//       path: "../../app/font/BMJUA_ttf.ttf",
+//       weight: "normal",
+//       style: "normal",
+//     },
+//   ],
+// });
+
+export function ChakraLayout({ children }: { children: React.ReactNode }) {
+  const config: ThemeConfig = {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  };
+
+  //   const [mount, setMount] = useState(false);
+
+  //   useLayoutEffect(() => {
+  //     setMount(true);
+  //   }, []);
+
+  //   if (!mount) {
+  //     return <></>;
+  //   }
+
+  const theme = extendTheme({
+    config,
+    // fonts: {
+    //   body: BMJUA.style.fontFamily,
+    // },
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode="system" />
+      <CacheProvider>{children}</CacheProvider>
+    </ChakraProvider>
+  );
+}
