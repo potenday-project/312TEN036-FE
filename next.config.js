906effd24ad2525690a5d76b1962902a36687eb4
최현우 <hw.choi@okestro.com>
2023-12-14 21:56:16 +0900

    feat(OAuth) : 카카오 소셜 로그인
    Refs:[#31]

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination:
-          "http://be-be-c957f-21216619-aeb7ba37580c.kr.lb.naverncp.com/",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination:
+  //         "http://be-be-c957f-21216619-aeb7ba37580c.kr.lb.naverncp.com/",
+  //     },
+  //   ];
+  // },
 
   images: {
     domains: ["images.pexels.com"],
